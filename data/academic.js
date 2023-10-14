2f6f0d8d27d7ae94c484dcd7197db6f010225a76
tiagodgy <tiagogodoymoreira@gmail.com>
2023-10-14 15:36:26 -0300

    Update About page to Sobre

---
+++ data/academic.js
@@ -0,0 +1,20 @@
+const academic = [
+  {
+    jobTitle: 'Tecnólogo',
+    company: 'Faculdade XPe',
+    companyUrl: '',
+    startDate: '2022-10-01',
+    endDate: '2025-03-01',
+    location: 'São Paulo, Brasil',
+  },
+  {
+    jobTitle: 'Bacharelado em Engenharia de Software',
+    company: 'Centro Universitário Internacional',
+    companyUrl: '',
+    startDate: '2021-03-01',
+    endDate: '2026-03-01',
+    location: 'São Paulo, Brasil',
+  },
+]
+
+export default academic
--- pages/sobre.js
+++ pages/sobre.js
@@ -4,24 +4,20 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { parseISO, format, intervalToDuration } from 'date-fns'
 import Base from '../layouts/Base'
-import { ButtonPrimary } from '../components/ButtonPrimary'
-import Pronunciation from '../components/Pronunciation'
 import Toast from '../components/Toast'
 import stripHtml from '../lib/strip-html'
 import items from '../data/about'
-import Lottie from 'lottie-react'
-import copyBioIcon from '../public/static/icons/copy-bio.json'
-import downloadIcon from '../public/static/icons/download.json'
+import academic from '../data/academic'
 
 export async function getStaticProps() {
   const meta = {
-    title: 'About // Zeno Rocha',
+    title: 'Sobre Tiago Godoy',
     description:
-      "Zeno Rocha is a Brazilian creator and programmer. He currently lives in San Francisco, California, where he's the Founder & CEO at Resend. His lifelong appreciation for building software and sharing knowledge led him to speak in over 110 conferences worldwide. His passion for open source put him on the top 20 most active users on GitHub at age 22. Before moving to the US, Zeno developed multiple applications, mentored startups, and worked at major companies in Latin America, such as Globo and Petrobras.",
-    tagline: 'Create. Share. Repeat.',
-    image: '/static/images/about-bw.jpg',
-    primaryColor: 'pink',
-    secondaryColor: 'purple',
+      'Meu nome é Tiago Godoy, iniciei minha carreira trabalhando como desenvolvedor de software na SmartGateway. Atualmente sou estagiário de Engenharia de Software e graduando em Defesa Cibernética na Faculdade XPe.',
+    tagline: 'Sonho Grande. Mente Aberta.',
+    image: '/static/images/expert-xp.jpg',
+    primaryColor: 'yellow',
+    secondaryColor: 'pink',
   }
 
   return { props: meta }
@@ -32,16 +28,14 @@ function Sobre(props) {
   const [toastTitle, setToastTitle] = React.useState('')
   const [toastDescription, setToastDescription] = React.useState('')
   const [showToast, setShowToast] = React.useState(false)
-  const copyBioRef = React.useRef()
-  const downloadRef = React.useRef()
 
   const renderIntro = () => {
     return (
       <Container>
         <Section>
           <Image
-            alt="Zeno"
-            src="/static/images/zeno-bw.jpg"
+            alt="Tiago"
+            src="/static/images/expert-xp.jpg"
             width="336"
             height="336"
             placeholder="blur"
@@ -56,88 +50,58 @@ function Sobre(props) {
               '@bp2': { marginTop: '-6px' },
             }}
           >
-            <strong>Hey, I'm Zeno Rocha</strong>
-            <Pronunciation />I started as a software engineer back in 2009,
-            working with Flash.
+            <strong>Olá, eu sou Tiago Godoy.</strong> Iniciei minha carreira
+            trabalhando como desenvolvedor de software na SmartGateway.
+            Atualmente sou estagiário de Engenharia de Software e graduando em
+            Defesa Cibernética na Faculdade XPe, tendo sido o{' '}
+            <strong>segundo</strong> colocado no processo seletivo com{' '}
+            <strong>127 mil participantes</strong>.
           </Paragraph>
           <Paragraph>
-            I'm the <strong>Founder & CEO</strong> at Resend. Before that, I was
-            a VP of Developer Experience at WorkOS and CPO at Liferay Cloud. I'm
-            originally from Brazil and now living in{' '}
-            <strong>San Francisco, California</strong> with my amazing wife and
-            beautiful daughter.
+            <strong>Sonho Grande. Mente Aberta. Espírito Empreendedor.</strong>{' '}
+            Esses são os valores da Faculdade XPe que adotei e sempre os levarei
+            comigo. Sonhar pequeno e grande requer o mesmo esforço,{' '}
+            <strong>Sonhe Grande</strong>.
           </Paragraph>
           <Paragraph>
-            <strong>I love dark mode</strong>, open source, and side projects.
-            When I'm not working, I like running, watching movies, and{' '}
-            <strong>eating cheese</strong>.
+            <strong>No meu tempo livre</strong> eu gosto de trabalhar em
+            projetos pessoais, ir na academia, andar de bicicleta, assistir
+            filmes e jogar video-game.
           </Paragraph>
         </Section>
       </Container>
     )
   }
 
-  const renderBio = () => {
-    const btnStyle = {
-      display: 'inline-flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }
-    const iconStyle = { width: 24, height: 24, marginRight: 8 }
-
-    return (
-      <div>
-        <p>
-          This is made for journalists, podcast hosts, and event organizers to
-          copy-and-paste.
-        </p>
-        <blockquote>
-          <p>{description}</p>
-        </blockquote>
-        <ButtonsContainer>
-          <ButtonPrimary
-            as="button"
-            style={btnStyle}
-            onClick={copyBio}
-            onMouseEnter={() => copyBioRef.current?.play()}
-            onMouseLeave={() => copyBioRef.current?.stop()}
-          >
-            <Lottie
-              lottieRef={copyBioRef}
-              style={iconStyle}
-              animationData={copyBioIcon}
-              loop={false}
-              autoplay={false}
-            />
-            Copy Bio
-          </ButtonPrimary>
-          <span style={{ margin: '0 20px 0 10px' }}>•</span>
-          <ButtonPrimary
-            as="a"
-            download
-            role="button"
-            href="/static/images/zeno.png"
-            style={btnStyle}
-            onClick={downloadHeadshot}
-            onMouseEnter={() => downloadRef.current?.play()}
-            onMouseLeave={() => downloadRef.current?.stop()}
-          >
-            <Lottie
-              lottieRef={downloadRef}
-              style={iconStyle}
-              animationData={downloadIcon}
-              loop={false}
-              autoplay={false}
-            />
-            Download Headshot
-          </ButtonPrimary>
-        </ButtonsContainer>
-      </div>
-    )
+  const renderCarreer = () => {
+    return items.map((item, index) => {
+      return (
+        <div style={{ marginBottom: 40 }} key={index}>
+          <h3>{item.jobTitle}</h3>
+          <p style={{ margin: 0 }}>
+            <a href={item.companyUrl} target="_blank">
+              {item.company}
+            </a>
+            <span> • {item.location}</span>
+          </p>
+          <p style={{ margin: 0 }}>
+            <span>{format(parseISO(item.startDate), 'LLL yyyy')}</span>
+            <span> – </span>
+            <span>
+              {item.endDate
+                ? format(parseISO(item.endDate), 'LLL yyyy')
+                : 'Present'}
+            </span>
+            <span> • </span>
+            <span>{getDuration(item.startDate, item.endDate)}</span>
+          </p>
+        </div>
+      )
+    })
   }
 
-  const renderAll = () => {
-    return items.map((item, index) => {
+  const renderAcademic = () => {
+    return academic.map((item, index) => {
       return (
         <div style={{ marginBottom: 40 }} key={index}>
           <h3>{item.jobTitle}</h3>
@@ -182,21 +146,6 @@ function Sobre(props) {
     return durationStr
   }
 
-  const downloadHeadshot = () => {
-    setToastTitle('Downloading...')
-    setToastDescription('You can now add this photo to your fancy site.')
-    setShowToast(true)
-  }
-
-  const copyBio = e => {
-    e.preventDefault()
-    navigator.clipboard.writeText(description)
-
-    setToastTitle('Copied :D')
-    setToastDescription('You can now paste it anywhere.')
-    setShowToast(true)
-  }
-
   return (
     <>
       <Head>
@@ -204,17 +153,16 @@ function Sobre(props) {
         <meta content={title} property="og:title" />
         <meta content={stripHtml(description)} name="description" />
         <meta content={stripHtml(description)} property="og:description" />
-        <meta content="https://zenorocha.com/about" property="og:url" />
-        <meta content={`https://zenorocha.com${image}`} property="og:image" />
+        <meta content="https://tiagogodoy.com/sobre" property="og:url" />
+        <meta content={`https://tiagogodoy.com${image}`} property="og:image" />
       </Head>
 
       {renderIntro()}
 
-      <h2>Bio</h2>
-      {renderBio()}
-
-      <h2>Career</h2>
-      {renderAll()}
+      <h2>Carreira</h2>
+      {renderCarreer()}
+      <h2>Acadêmico</h2>
+      {renderAcademic()}
 
       <Toast
         title={toastTitle}
@@ -238,7 +186,7 @@ const Paragraph = styled('p', {
   '@bp2': { margin: '15px 0' },
 })
 
-const ButtonsContainer = styled('p', {
+const ButtonsContainer = styled('div', {
   display: 'flex',
   alignItems: 'center',
 })
