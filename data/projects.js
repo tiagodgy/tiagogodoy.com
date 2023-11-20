75aef352a46620348959f0a19602b01c5785fc3f
tiagodgy <tiagogodoymoreira@gmail.com>
2023-11-20 18:38:37 -0300

    Update projects detail

--- data/projects.js
+++ data/projects.js
@@ -1,23 +1,23 @@
 const projects = [
   {
-    title: 'NRF24 PCB for Flipper Zero',
+    title: 'Foca - Block Distracting Websites',
     description:
-      'PCB criada utilizando EDA (Eletronic Design Automation). O circuito NRF24 em conjunto com o Flipper Zero permite explorar vulnerabilidade em dispositivos sem fio e executar o sequestro do dispositvo. Conhecido como mousejack, a vulnerabilidade foi descoberta em 2016 e ainda existem dispositivos vulneráveis sendo vendidos.',
-    image: '/static/img/projects/nrf24_pcb1.png',
-    url: '',
-    github: 'https://github.com/tiagodgy/flipper/tree/main/nrf24_pcb',
+      'Foca é uma extensão gratuita para Google Chrome, projetada para ajudar você a eliminar distrações na web. Além do recurso convencional de bloqueio de sites encontrado em muitas outras extensões, o Foca introduz um modo inovador chamado "Foco nos Estudos". Esse modo exclusivo permite que os usuários bloqueiem tudo, exceto o site designado para estudo, proporcionando um ambiente ideal para aprendizado concentrado e ininterrupto.',
+    image: '/static/img/projects/foca_extension.png',
+    url: 'https://chromewebstore.google.com/',
+    github: '',
     playStore: '',
     appleStore: '',
     year: '2023',
     active: true,
   },
   {
-    title: 'Foca - Block Distracting Websites',
+    title: 'NRF24 PCB for Flipper Zero',
     description:
-      'Foca é um aplicativo gratuito projetado para ajudar você a eliminar distrações de sites. Além do recurso convencional de bloqueio de sites encontrado em muitas outras extensões, o Foca introduz um modo inovador chamado "Foco nos Estudos". Esse modo exclusivo permite que os usuários bloqueiem tudo, exceto o site designado para estudo, proporcionando um ambiente ideal para aprendizado concentrado e ininterrupto.',
-    image: '/static/img/projects/foca_extension.png',
-    url: 'https://chromewebstore.google.com/',
-    github: '',
+      'PCB criada utilizando EDA (Eletronic Design Automation). O circuito NRF24 em conjunto com o Flipper Zero permite explorar vulnerabilidade em dispositivos sem fio e executar o sequestro do dispositvo. Conhecido como mousejack, a vulnerabilidade foi descoberta em 2016 e ainda existem dispositivos vulneráveis sendo vendidos.',
+    image: '/static/img/projects/nrf24_pcb1.png',
+    url: '',
+    github: 'https://github.com/tiagodgy/flipper/tree/main/nrf24_pcb',
     playStore: '',
     appleStore: '',
     year: '2023',
